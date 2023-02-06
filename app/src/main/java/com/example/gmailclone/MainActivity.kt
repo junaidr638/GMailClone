1b57b93fc3eb5e8c830bb62bf4b3af5b59143e48
Junaid <junaidr638@gmail.com>
2023-02-07 00:19:16 +0530

    Added Drawer Menu / OnClick not working

--- app/src/main/java/com/example/gmailclone/MainActivity.kt
+++ app/src/main/java/com/example/gmailclone/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.gmailclone.components.GmailDrawerMenu
 import com.example.gmailclone.components.HomeAppBar
 import com.example.gmailclone.ui.theme.GMailCloneTheme
 
@@ -30,7 +33,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GmailApp() {
-    Scaffold(topBar = { HomeAppBar() }) {
+    val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
+    val scrollState = rememberScrollState()
+    Scaffold(topBar = { HomeAppBar(scaffoldState,coroutineScope) },
+              drawerContent = {
+                  GmailDrawerMenu(scrollState = scrollState)
+              }) {
         it
     }
 }
--- app/src/main/java/com/example/gmailclone/components/HomeAppBar.kt
+++ app/src/main/java/com/example/gmailclone/components/HomeAppBar.kt
@@ -3,28 +3,31 @@ package com.example.gmailclone.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.gmailclone.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
 
 @Composable
-fun HomeAppBar() {
+fun HomeAppBar(scaffoldState: ScaffoldState, coroutineScope: CoroutineScope) {
+
 
     Box(modifier = Modifier.padding(10.dp)) {
 
         Card(
             modifier = Modifier.requiredHeight(50.dp),
-            shape = RoundedCornerShape(20.dp)
+            shape = RoundedCornerShape(20.dp),
+            elevation = 10.dp,
+            backgroundColor = Color(242, 242, 242)
         ) {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
@@ -32,17 +35,26 @@ fun HomeAppBar() {
                     .fillMaxSize()
                     .padding(8.dp)
             ) {
-                IconButton(onClick = { /*TODO*/ }) {
+                IconButton(onClick = {
+                    coroutineScope.launch {
+                        scaffoldState.drawerState.open()
+                    }
+                }
+                ) {
                     Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
                 }
 
                 Text(
-                    text = "Search in emails",
-                    modifier = Modifier.weight(2.0f)
+                    text = "Search in emails...",
+                    modifier = Modifier.weight(2.0f),
+                    color = Color.Gray
                 )
 
-                Image(painter = painterResource(id = R.drawable.gmailiconpicture),
-                       contentDescription = "Profiel Picture")
+                Image(
+                    painter = painterResource(id = R.drawable.gmailiconpicture),
+                    modifier = Modifier.size(80.dp),
+                    contentDescription = "Profile Picture"
+                )
 
 
             }
@@ -50,10 +62,3 @@ fun HomeAppBar() {
     }
 }
 
-
-@Preview
-@Composable
-
-fun AppBarPreview() {
-    HomeAppBar()
-}
