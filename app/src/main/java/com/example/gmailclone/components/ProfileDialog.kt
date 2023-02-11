636d28ec29fe96bb4d56fdc92f9632928da96255
Junaid <junaidr638@gmail.com>
2023-02-11 14:44:37 +0530

    Added ProfileDialog - final

--- app/src/main/java/com/example/gmailclone/components/ProfileDialog.kt
+++ app/src/main/java/com/example/gmailclone/components/ProfileDialog.kt
@@ -2,6 +2,7 @@ package com.example.gmailclone.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -9,12 +10,15 @@ import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material.icons.outlined.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,7 +29,9 @@ import com.example.gmailclone.R
 
 @Composable
 fun AccountsDialog(openDialog: MutableState<Boolean>) {
-    Dialog(onDismissRequest = { /*TODO*/ }) {
+    Dialog(onDismissRequest = { openDialog.value = false}) {
+
+        ProfileDialogUI()
 
     }
 }
@@ -34,7 +40,7 @@ fun AccountsDialog(openDialog: MutableState<Boolean>) {
 fun ProfileDialogUI() {
     Card(
         backgroundColor = Color.White,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.wrapContentHeight(Alignment.CenterVertically)
     ) {
         Column(modifier = Modifier.padding(16.dp)) {
             Row(modifier = Modifier.padding(2.dp)) {
@@ -54,7 +60,7 @@ fun ProfileDialogUI() {
                 )
             }
 
-            Row(modifier = Modifier.padding(bottom = 12.dp)) {
+            Row(modifier = Modifier.padding()) {
                 Image(
                     painter = painterResource(id = R.drawable.profile_icon),
                     contentDescription = "Profile Icon",
@@ -70,21 +76,23 @@ fun ProfileDialogUI() {
 
                     Text(
                         text = "junaidr638@gmail.com",
-                        fontWeight = FontWeight.Light
+                        fontWeight = FontWeight.Light,
+                        fontSize = 12.sp
                     )
                 }
-                Spacer(modifier = Modifier.width(130.dp))
+                Spacer(modifier = Modifier.width(80.dp))
                 Text(
                     text = "99+",
                     modifier = Modifier.padding(top = 27.dp),
-                    color = Color.Gray
+                    color = Color.Gray,
+                    fontSize = 12.sp
 
 
                 )
             }
 
             Row(modifier = Modifier.padding(bottom = 20.dp)) {
-                Spacer(modifier = Modifier.width(75.dp))
+                Spacer(modifier = Modifier.width(40.dp))
                 Text(
                     text = "  Manage your Google Account  ",
                     modifier = Modifier
@@ -95,7 +103,66 @@ fun ProfileDialogUI() {
                 )
             }
 
-            Divider()
+            Divider(modifier = Modifier.padding(bottom = 20.dp))
+
+            Row(modifier = Modifier.padding(bottom = 15.dp, start = 15.dp)) {
+                Icon(imageVector = Icons.Outlined.Cloud, contentDescription = "Cloud Icon")
+                Spacer(modifier = Modifier.width(30.dp))
+                Text(text = "Storage used: 54% of 100 GB",
+                    fontSize = 12.sp)
+            }
+
+            Divider(modifier = Modifier.padding(bottom = 20.dp))
+
+            Column {
+
+                Row(modifier = Modifier.padding(bottom = 20.dp, start = 15.dp)) {
+                    Icon(
+                        imageVector = Icons.Outlined.PersonAddAlt,
+                        contentDescription = "Add another account"
+                    )
+
+                    Spacer(modifier = Modifier.width(30.dp))
+                    Text(
+                        text = "Add another account",
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 16.sp
+                    )
+                }
+
+                Row(modifier = Modifier.padding(start = 15.dp, bottom = 15.dp)) {
+                    Icon(
+                        imageVector = Icons.Outlined.ManageAccounts,
+                        contentDescription = "Manage accounts on this device"
+                    )
+
+                    Spacer(modifier = Modifier.width(30.dp))
+
+                    Text(
+                        text = "Manage accounts on this device",
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 16.sp
+                    )
+
+
+                }
+
+            }
+
+            Divider(modifier = Modifier.padding(bottom = 20.dp))
+
+            Row(modifier = Modifier.padding(bottom = 8.dp)) {
+                Spacer(modifier = Modifier.width(30.dp))
+                Text(text = "Privacy Policy", fontSize = 12.sp)
+                Spacer(modifier = Modifier.width(20.dp))
+                Text(text = "·",
+                    fontWeight = FontWeight.ExtraBold,
+                    fontSize = 16.sp,
+                    )
+                Spacer(modifier = Modifier.width(20.dp))
+                Text(text = "Terms of service", fontSize = 12.sp)
+            }
+
 
         }
     }
